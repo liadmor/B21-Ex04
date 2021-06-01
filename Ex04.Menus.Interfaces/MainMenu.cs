b98afda701b73e1bb8287d6309a92891128590be
liadmor <liadmor96@gmail.com>
2021-06-01 15:44:07 +0300

    Add project files.

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu
+    {
+        List<MenuItem> m_MenuItems;
+        string m_MenuTitle;
+
+        public MainMenu(string i_menuTitle)
+        {
+            m_MenuTitle = i_menuTitle;
+        }
+
+        public void AddMenuItem(MenuItem i_MenuItemToAdd)
+        {
+            m_MenuItems.Add(i_MenuItemToAdd);
+        }
+
+        public void print 
+
+
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public abstract class MenuItem
+    {
+        private string m_MenuItemName;
+
+        public string MenuItemName
+        {
+            get
+            {
+                return m_MenuItemName;
+            }
+            set
+            {
+                m_MenuItemName = value;
+            }
+        }
+
+    }
+}
